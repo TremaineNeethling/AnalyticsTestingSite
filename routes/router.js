30500c2c3f4fa61e472768154c924fdb98afd3a8
Tremaine Neethling <tremaine.neethling@mechanical-ink.co.za>
2020-01-02 11:48:42 +0200

    adds links to help switch between pages

--- routes/router.js
+++ routes/router.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.sendfile("./static/index.html");
 });
 
+router.get("/explanation", (req, res) => {
+  res.sendfile("./static/explanation.html");
+});
+
 module.exports = router;
